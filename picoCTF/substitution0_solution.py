42aa3bc3b9337fa27b917d7cd3b68978d42da964
whoamiPwns1729 <mishraanish191@gmail.com>
2024-08-19 23:46:19 +0530

    Update substitution0_solution.py

--- picoCTF/substitution0_solution.py
+++ picoCTF/substitution0_solution.py
@@ -1,1 +1,34 @@
+#!/usr/bin/python3
+key = "DECKFMYIQJRWTZPXGNABUSOLVH"
+string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+flag = []
+for i in "xqcpCBM":
+    if i.islower():
+        flag.append(string[key.index(i.upper())].lower())
+    else:
+        flag.append(string[key.index(i)])
+
+flag.append('{')
+for i in "5UE5717U710Z":
+    if i.isnumeric():
+        flag.append(i)
+    else:
+        flag.append(string[key.index(i)])
+
+flag.append('_')
+for i in "3S0WU710Z":
+    if i.isnumeric():
+        flag.append(i)
+    else:
+        flag.append(string[key.index(i)])
+
+flag.append('_')
+for i in "59533D2F":
+    if i.isnumeric():
+        flag.append(i)
+    else:
+        flag.append(string[key.index(i)])
+
+flag.append('}')
+print(f"\n[*] flag : {''.join(flag)}\n")
